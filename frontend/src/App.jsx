2b8dd40c7b227f40f0f67af804caa57f0e62c806
MohammedAldahash <mohom3535@gmail.com>
2025-04-19 18:56:33 +0300

    app and navbar restor before problems

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import LoginPage from "./pages/LoginPage";
 import SignInPage from "./pages/SignInPage";
-<<<<<<< HEAD
 import ChallengesPage from "./pages/Challenges";
 import ProfilePage from "./pages/ProfilePage";
 import LandingPage from "./pages/LandingPage";
@@ -10,19 +9,10 @@ import ChatPage from "./pages/ChatPage";
 import ChallengeCoachView from "./pages/ChallengeCoachView";
 
 import Leaderboard from "./pages/LeaderBoard";
-=======
-import ProfilePage from "./pages/ProfilePage";
-import LiftSideDash from "./pages/LiftSideDash";
-import ActiveChallenges from "./pages/ActiveChallenges";
-import Dashboard from "./pages/Dashboard";
-import WeeklyProgress from "./pages/WeeklyProgress";
-import Navbar from "./pages/Navbar";
->>>>>>> abdullah/dashboard
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
@@ -35,9 +25,6 @@ function App() {
           <Route path="/leaderboard" element={<Leaderboard/>}/>
         </Routes>
       </BrowserRouter>
-=======
-      <Dashboard />
->>>>>>> abdullah/dashboard
     </div>
   );
 }
